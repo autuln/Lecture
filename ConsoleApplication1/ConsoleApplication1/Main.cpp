c51540188976dec5e4d07673798885f184c97b40
ryu seong min <autuln@awesomepig.com>
2016-05-04 15:47:44 +0900

    Add 함수 추가

--- ConsoleApplication1/ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/ConsoleApplication1/Main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+int add(int _nInputA, int _nInputB);
 
 
 int main()
 {
 
 	std::cout << "hellow world" << std::endl;
+}
+
+int add(int _nInputA, int _nInputB)
+{
+	return _nInputA + _nInputB;
 }
